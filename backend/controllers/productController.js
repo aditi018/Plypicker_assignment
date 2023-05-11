c9f36da1ef1160f2f66c676fdcc5bfeb2c04ad7b
aditi018 <dasaditi018@gmail.com>
2023-05-11 16:39:06 +0530

    first commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,11 +5,12 @@ const Category = require("../models/categoryModel");
 const Subcategory = require("../models/subCategoryModel");
 const catchAsyncError = require("../middleware/catchAsyncError");
 const ApiFeatures = require("../utils/apiFeatures");
+const ErrorHandler = require("../utils/errorHandler");
 
 exports.getProducts = catchAsyncError(
     async(req,res) => {
 
-        const resultPerPage = 12;
+        const resultPerPage = 30;
         const apifeatures = new ApiFeatures(Product.find().
         populate('group').populate('brand').populate('category').populate('subcategory')
         ,req.query).pagination(resultPerPage);
@@ -19,6 +20,24 @@ exports.getProducts = catchAsyncError(
         res.status(200).json({
             success:true,
             products,
+        })
+    }
+)
+
+exports.getProductDetail = catchAsyncError(
+    async(req,res,next) =>{
+        const category_name = req.body.name;
+        const category_id = await Category.find({name: category_name});
+        
+        const product_id = await Product.find({category: category_id[0]._id}).populate('category').populate('subcategory').populate('brand').populate('group');
+        
+        console.log(product_id.length);
+        if(product_id.length == 0){
+            return next(new ErrorHandler("Product not found",404));
+        }
+        res.status(200).json({
+            success:true,
+            product_id,
         })
     }
 )
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,7 @@
-const {getProducts} = require("../controllers/productController");
+const {getProducts,getProductDetail} = require("../controllers/productController");
 
 const router = require("express").Router();
 router.route("/products").get(getProducts);
+router.route("/product/detail").get(getProductDetail);
 
 module.exports = router;
